d4978495bd95b2c235d940bf0d71c3cb4401d9bc
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-03-04 10:11:34 -0500

    Initial commit

---
+++ game.py
@@ -0,0 +1,39 @@
+# Katelyn Curtiss
+# February 12 2025
+# Game Code
+
+import pygame
+import sys
+import config
+
+def init_game ():
+    pygame.init()
+    screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
+        
+    pygame.display.set_caption(config.TITLE)
+    return screen
+
+def handle_events ():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return False
+        return True
+
+def main():
+    screen = init_game()
+    clock = pygame.time.Clock() # Initialize the clock here
+    running = True
+    while running:
+        running = handle_events()
+    screen.fill(config.WHITE) # Use color from config
+    pygame.display.flip()
+
+    clock.tick(config.FPS) 
+pygame.quit()
+
+sys.exit()
+if __name__ == "__main__":
+    main()
